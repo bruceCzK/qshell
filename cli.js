afa45d24e21e0973170c640fc1245c8fca0518c5
叫我布鲁斯 <bruceczk@foxmail.com>
2016-07-04 12:02:21 +0800

    Use spawn instead of execFile to fix prompt input issue; Change cwd to homedir

--- cli.js
+++ cli.js
@@ -4,7 +4,7 @@ const os = require('os');
 const platform = os.platform();
 const arch = os.arch();
 const userArgs = process.argv.slice(2);
-const execFile = require('child_process').execFile;
+const spawn = require('child_process').spawn;
 const version = require('./package.json').version;
 
 const bin = __dirname + '/bin/qshell_' + platform + '_' + (arch === 'x64' ? 'amd64' : '386');
@@ -12,14 +12,7 @@ const bin = __dirname + '/bin/qshell_' + platform + '_' + (arch === 'x64' ? 'amd
 if (userArgs[0] === '-v') {
   console.log('v' + version);
 }
-execFile(bin, userArgs, function (err, stdout, stderr) {
-  if (err) {
-    throw err;
-  }
-  if (stdout) {
-    console.log(stdout);
-  }
-  if (stderr) {
-    console.error(stderr);
-  }
+spawn(bin, userArgs, {
+  cwd: os.homedir(),
+  stdio: 'inherit'
 });
