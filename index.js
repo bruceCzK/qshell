d715cf2354ea50ebc93c431fd4436ddf2ca17162
叫我布鲁斯 <bruceczk@foxmail.com>
2016-07-04 15:14:09 +0800

    Add exec and login function for module

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
 #! /usr/bin/env node
+'use strict';
 
 const os = require('os');
 const platform = os.platform();
 const arch = os.arch();
-const userArgs = process.argv.slice(2);
 const spawn = require('child_process').spawn;
+const execSync = require('child_process').execSync;
 
 const bin = './bin/qshell_' + platform + '_' + (arch === 'x64' ? 'amd64' : '386');
 
-module.exports = function (cb) {
+module.exports = {
+  exec: exec,
+  login: login
+};
+
+function login(appId, secrect) {
+  return execSync(bin, [appId, secrect], {
+    cwd: os.homedir()
+  });
+}
+
+function exec(command, args, cb) {
   if (typeof cb !== 'function') {
     throw new Error('Callback is not a function');
   }
-  spawn(bin, userArgs, {
+  spawn(bin, [command, ...args], {
     cwd: os.homedir()
   }).on('exit', cb);
-};
+}
