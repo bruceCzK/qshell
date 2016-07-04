8a8bccde1e99204969f90ea20eeed6be9b01aebb
叫我布鲁斯 <bruceczk@foxmail.com>
2016-07-04 12:02:21 +0800

    Use spawn instead of execFile to fix prompt input issue; Change cwd to homedir

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const os = require('os');
 const platform = os.platform();
 const arch = os.arch();
 const userArgs = process.argv.slice(2);
-const execFile = require('child_process').execFile;
+const spawn = require('child_process').spawn;
 
 const bin = './bin/qshell_' + platform + '_' + (arch === 'x64' ? 'amd64' : '386');
 
@@ -12,5 +12,7 @@ module.exports = function (cb) {
   if (typeof cb !== 'function') {
     throw new Error('Callback is not a function');
   }
-  return execFile(bin, userArgs, cb);
+  spawn(bin, userArgs, {
+    cwd: os.homedir()
+  }).on('exit', cb);
 };
