297bcace22ab331acde8a7710a7ae5af7a21746f
叫我布鲁斯 <bruceczk@foxmail.com>
2016-07-04 10:32:46 +0800

    Add package version output

--- cli.js
+++ cli.js
@@ -5,9 +5,13 @@ const platform = os.platform();
 const arch = os.arch();
 const userArgs = process.argv.slice(2);
 const execFile = require('child_process').execFile;
+const version = require('./package.json').version;
 
 const bin = __dirname + '/bin/qshell_' + platform + '_' + (arch === 'x64' ? 'amd64' : '386');
 
+if (userArgs[0] === '-v') {
+  console.log('v' + version);
+}
 execFile(bin, userArgs, function (err, stdout, stderr) {
   if (err) {
     throw err;
