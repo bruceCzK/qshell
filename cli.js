ad20f0fa2c355e5e2fe7fda5a671aa05b525ca15
陈卓恺 <bruceczk@foxmail.com>
2018-12-04 14:40:09 +0800

    Update bin files

--- cli.js
+++ cli.js
@@ -1,17 +1,21 @@
 #! /usr/bin/env node
 
-const os = require('os');
-const platform = os.platform();
-const arch = os.arch();
-const userArgs = process.argv.slice(2);
-const spawn = require('child_process').spawn;
-const version = require('./package.json').version;
+const os = require('os')
+const path = require('path')
+const arch = os.arch()
+const userArgs = process.argv.slice(2)
+const spawn = require('child_process').spawn
+const version = require('./package.json').version
+const platform = {
+  win32: 'windows',
+  darwin: 'darwin'
+}[os.platform()] || 'linux'
 
-const bin = __dirname + '/bin/qshell_' + platform + '_' + (arch === 'x64' ? 'amd64' : '386');
+const bin = path.join(__dirname, 'bin', 'qshell_' + platform + '_' + arch)
 
 if (userArgs[0] === '-v') {
-  console.log('v' + version);
+  console.log('v' + version)
 }
 spawn(bin, userArgs, {
   stdio: 'inherit'
-});
+})
--- index.js
+++ index.js
@@ -1,26 +1,30 @@
 #! /usr/bin/env node
-'use strict';
+'use strict'
 
-const os = require('os');
-const platform = os.platform();
-const arch = os.arch();
-const spawn = require('child_process').spawn;
-const execSync = require('child_process').execSync;
+const os = require('os')
+const path = require('path')
+const arch = os.arch()
+const spawn = require('child_process').spawn
+const execSync = require('child_process').execSync
+const platform = {
+  win32: 'windows',
+  darwin: 'darwin'
+}[os.platform()] || 'linux'
 
-const bin = './bin/qshell_' + platform + '_' + (arch === 'x64' ? 'amd64' : '386');
+const bin = path.join(__dirname, 'bin', 'qshell_' + platform + '_' + arch)
 
 module.exports = {
   exec: exec,
   login: login
-};
+}
 
 function login(appId, secret) {
-  return execSync(bin, [appId, secret]);
+  return execSync(bin, [appId, secret])
 }
 
 function exec(command, args, cb) {
   if (typeof cb !== 'function') {
-    throw new Error('Callback is not a function');
+    throw new Error('Callback is not a function')
   }
-  spawn(bin, [command, ...args]).on('exit', cb);
+  spawn(bin, [command, ...args]).on('exit', cb)
 }
