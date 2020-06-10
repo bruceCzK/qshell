6022347e52aa4ade748f47cfa64282f483a41adb
机智的布鲁斯 <bruceczk@foxmail.com>
2020-06-10 17:08:55 +0800

    Remove login method

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
-#! /usr/bin/env node
-'use strict'
-
 const os = require('os')
 const path = require('path')
 const arch = os.arch()
-const spawn = require('child_process').spawn
-const execSync = require('child_process').execSync
+const {spawn} = require('child_process')
 const platform = {
   win32: 'windows',
   darwin: 'darwin'
@@ -13,16 +9,7 @@ const platform = {
 
 const bin = path.join(__dirname, 'bin', 'qshell-' + platform + '-' + arch)
 
-module.exports = {
-  exec: exec,
-  login: login
-}
-
-function login(appId, secret) {
-  return execSync(bin, [appId, secret])
-}
-
-function exec(command, args, cb) {
+module.exports = function exec(command, args, cb) {
   if (typeof cb !== 'function') {
     throw new Error('Callback is not a function')
   }
